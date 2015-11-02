43dc0d021072bc6535a903e7fb5d73245b9ea794
machi <machi0545@hotmail.com>
2015-11-02 23:05:07 +0900

    First commit

--- app/src/main/java/com/example/machi/helloworld/HelloWorld.java
+++ app/src/main/java/com/example/machi/helloworld/HelloWorld.java
@@ -3,14 +3,19 @@ package com.example.machi.helloworld;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Window;
+import android.widget.LinearLayout;
+
 
 public class HelloWorld extends Activity {
     @Override
     public void onCreate(Bundle bundle) {
         super.onCreate(bundle);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
-        setContentView(new HelloView(this));
+        LinearLayout myLayout = new LinearLayout(this);
+        LinearLayout secondLayout = new LinearLayout(this);
+        myLayout.addView(new HelloView(this));
+        secondLayout.addView(new HelloView(this));
+        myLayout.addView(secondLayout);
+        setContentView(myLayout);
     }
-
-
 }
