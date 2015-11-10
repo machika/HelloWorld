620dc00f195a67461ec40e8f99aac2198e6ad7c9
machi <machi0545@hotmail.com>
2015-11-10 13:47:41 +0900

    Lollipop

--- app/src/main/java/com/example/machi/helloworld/HelloView.java
+++ app/src/main/java/com/example/machi/helloworld/HelloView.java
@@ -4,20 +4,61 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Point;
 import android.view.View;
-import android.view.Window;
+import android.view.WindowManager;
+import android.view.Display;
+
+import java.util.GregorianCalendar;
+import java.util.Random;
 
 public class HelloView extends View {
+
+    private int myX, myY, myRadius, myRed, myGreen, myBlue;
+    public int myColor;
+
     public HelloView(Context context) {
         super(context);
         setBackgroundColor(Color.WHITE);
+        WindowManager wm = (WindowManager)getContext().getSystemService(Context.WINDOW_SERVICE);
+        Display disp = wm.getDefaultDisplay();
+        Point dispSize = new Point();
+        disp.getSize(dispSize);
+
+        myX = dispSize.x / 2;
+        myY = dispSize.y / 2;
+        myRadius = (myY > myX) ? myY / 3 : myX / 3;
+        myRed = 0;
+        myGreen = 0;
+        myBlue = 255;
+        myColor = Color.rgb(myRed, myGreen, myBlue);
+    }
+
+    public void ChangeColor() {
+        Random rand = new Random();
+        rand.setSeed(GregorianCalendar.getInstance().getTimeInMillis());
+        myRed = rand.nextInt() % 255;
+        myGreen = rand.nextInt() % 255;
+        myBlue = rand.nextInt() % 255;
+        myColor = Color.rgb(myRed, myGreen, myBlue);
+        invalidate();
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         Paint paint = new Paint();
-        paint.setTextSize(48);
-        canvas.drawText("Hello World!", 0, 48, paint);
+        paint.setAntiAlias(true);
+        paint.setStrokeWidth(10);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setColor(Color.BLACK);
+        canvas.drawCircle(myX, myY, myRadius, paint);
+
+        paint.setStyle(Paint.Style.FILL);
+        paint.setColor(myColor);
+        canvas.drawCircle(myX, myY, myRadius, paint);
+
+        paint.setColor(Color.BLACK);
+        canvas.drawLine(myX, myY+myRadius, myX, myY*2, paint);
     }
 
 }
--- app/src/main/java/com/example/machi/helloworld/HelloWorld.java
+++ app/src/main/java/com/example/machi/helloworld/HelloWorld.java
@@ -2,20 +2,26 @@ package com.example.machi.helloworld;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.widget.LinearLayout;
 
 
-public class HelloWorld extends Activity {
+public class HelloWorld extends Activity implements View.OnClickListener {
     @Override
     public void onCreate(Bundle bundle) {
         super.onCreate(bundle);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         LinearLayout myLayout = new LinearLayout(this);
-        LinearLayout secondLayout = new LinearLayout(this);
-        myLayout.addView(new HelloView(this));
-        secondLayout.addView(new HelloView(this));
-        myLayout.addView(secondLayout);
         setContentView(myLayout);
+        HelloView myView = new HelloView(this);
+        myView.setOnClickListener(this);
+        myLayout.addView(myView);
+    }
+
+    @Override
+    public void onClick(View v) {
+        HelloView myView = (HelloView)v;
+        myView.ChangeColor();
     }
 }
